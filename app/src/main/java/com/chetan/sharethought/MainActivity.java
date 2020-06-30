e973592f9831dcb51359aefa76c4eb413afb8da1
chetan <csj5483@gmail.com>
2020-06-30 16:42:58 +0530

    second commit

--- app/src/main/java/com/chetan/sharethought/MainActivity.java
+++ app/src/main/java/com/chetan/sharethought/MainActivity.java
@@ -1,14 +1,62 @@
 package com.chetan.sharethought;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        loadFragment(new HomeFragment());
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()) {
+            case R.id.home_icon:
+                fragment = new HomeFragment();
+                break;
+
+
+            case R.id.account_icon:
+                fragment = new ProfileFragment();
+                break;
+
+            case R.id.search_icon:
+                fragment = new SearchFragment();
+                break;
+            case R.id.upload_icon:
+                fragment = new UploadFragment();
+                break;
+            case R.id.trending_icon:
+                fragment = new TrendingFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frame_layout, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
